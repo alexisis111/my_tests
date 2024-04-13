c401527eced545b29444b513fd4d95083f4d4cea
alexisis111 <shuragilyow@yandex.ru>
2024-04-13 19:54:59 +0300

    init

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import Button from "../button/button";
 import useTelegram from "../../hooks/useTelegram";
-
+import './header.css'
 const Header = () => {
 
 const {onClose, user} = useTelegram()
